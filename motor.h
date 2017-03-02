578ae965f0c4b455d40d854ab48faa9f9a736c67
Louis-Marie MICHELIN <louis-marie.michelin@ensta-paristech.fr>
2017-03-02 20:50:46 +0100

    First working code for pi

--- motor.h
+++
@@ -1,7 +0,0 @@
-long readRightEncoder(void);								// Reads encoder data for both motors and displays to the screen
-long readLeftEncoder(void);								// Reads encoder data for both motors and displays to the screen
-void resetEncoders(void);									// Resets the encoders to 0
-void driveRightMotor(int speed);										// Drive the motors forwards
-void driveLeftMotor(int speed);										// Drive the motors forwards
-void stopRightMotor(void);										// Stop the motors
-void stopLeftMotor(void);										// Stop the motors
--- servo.h
+++
@@ -1,3 +0,0 @@
-void styloUp();
-void styloDown();
-void styloDetach();
